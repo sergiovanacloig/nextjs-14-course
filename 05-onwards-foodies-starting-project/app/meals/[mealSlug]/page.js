a752c585bd2f15c122f2be1f9db7ac4c94392641
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2024-05-15 13:23:48 +0200

    Add meal detail page

--- 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
+++ 05-onwards-foodies-starting-project/app/meals/[mealSlug]/page.js
@@ -1,8 +1,41 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
 export default function MealDetailPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  const { title, image, creator, creator_email, instructions, summary } = meal;
+  const instructionsFormatted = instructions.replace(/\n/g, "<br />");
+
+  console.log(instructions);
   return (
-    <main>
-      <h1>Meal detail page</h1>
-      <p>{params.mealSlug}</p>
-    </main>
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailo:${creator_email}`}>{creator}</a>
+          </p>
+          <p className={styles.summary}>{summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: instructionsFormatted,
+          }}
+        ></p>
+      </main>
+    </>
   );
 }
---
+++ 05-onwards-foodies-starting-project/app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function MealsError() {
+  return (
+    <main className="error">
+      <h1>An error occurred!</h1>
+      <p>Failed to fetch meal data. Pls try again later</p>
+    </main>
+  );
+}
