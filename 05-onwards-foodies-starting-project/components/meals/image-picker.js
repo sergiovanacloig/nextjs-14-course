e0f9f58f0b2821a961c10a0ea7c97f4309dde519
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2024-05-23 10:43:45 +0200

    Add meals form and image picker

---
+++ 05-onwards-foodies-starting-project/components/meals/image-picker.js
@@ -0,0 +1,62 @@
+"use client";
+import { useRef, useState } from "react";
+import Image from "next/image";
+
+import styles from "./image-picker.module.css";
+
+export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
+  const imageInputRef = useRef();
+
+  const handlePickClick = () => {
+    imageInputRef.current.click();
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  return (
+    <div className={styles.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={styles.controls}>
+        <div className={styles.preview}>
+          {pickedImage ? (
+            <Image src={pickedImage} fill alt="Picked image preview" />
+          ) : (
+            <p>No image picked yet</p>
+          )}
+        </div>
+        <input
+          required
+          className={styles.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInputRef}
+          onChange={handleImageChange}
+        />
+        <button
+          className={styles.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
+}
